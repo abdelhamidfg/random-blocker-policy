804c841f4bb4306339b96f2c014f8d9dc3980884
abdelhamid <abdelhamidfg@gmail.com>
2022-05-10 19:12:36 +0400

    Initial commit

---
+++ policies/random-blocker/1.0.0/random_blocker.lua
@@ -0,0 +1,34 @@
+local policy = require('apicast.policy')
+local _M = policy.new('random_blocker', '1.0.0')
+local default_error_message = "Request blocked due to random blocker policy"
+local new = _M.new
+
+function _M.new(config)
+    local self = new(config)
+    self.error_message = config.error_message or default_error_message
+    return self
+end
+
+ 
+
+local function deny_request(error_msg)
+    ngx.status = ngx.HTTP_FORBIDDEN
+    ngx.say(error_msg)
+    ngx.exit(ngx.status)
+end
+
+
+
+function _M:access(context)
+    local is_authorized = false
+    local random = math.random(1,1000)
+   
+    if (random % 2 == 0) then
+        is_authorized=true
+    end      
+    if not is_authorized then
+        return deny_request(self.error_message)
+    end
+end
+
+return _M
